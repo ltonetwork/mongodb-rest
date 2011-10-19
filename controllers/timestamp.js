9cb52ba3a9b7dc070042c51c4a9c327aa85e1bb0
makuchaku <makuchaku@adomado.com>
2011-10-19 14:46:26 +0530

    Corrected a stupid mistake

--- controllers/timestamp.js
+++ controllers/timestamp.js
@@ -5,7 +5,9 @@ exports.register = function(app) {
   // GET server timestamp
   app.get('/timestamp', function(req, res, next) {
     var date = new Date();
-    var response = {minutes : date.getMinutes(), hour : date.getHours(), day : date.getDate(), month : date.getMonth(), year : date.getFullYear(), sinceEpoch : date.getTime()};
+    // month is returned as 0-11 by JS, but by 1-12 by our API
+    // hour is returned as 0-23 by both JS and our API
+    var response = {minutes : date.getMinutes(), hour : date.getHours(), day : date.getDate(), month : (date.getMonth()+1), year : date.getFullYear(), sinceEpoch : date.getTime()};
     app.renderJsonpResponse(res, null, response);
   });
       
