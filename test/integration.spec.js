27b736809f5af26d67a7589cdc573ad03de785a7
ashleydavis <ashley@codecapers.com.au>
2014-07-08 07:50:31 +1000

    Added a test for retrieving a non-empty collection. Had to refactor to work with multiple tests, the server is only started once. It should be started and closed for each test but it will need to be refactored to support this.

--- test/integration.spec.js
+++ test/integration.spec.js
@@ -12,21 +12,25 @@ var collectionUrl = url + testDbName + '/' + testCollectionName;
 
 var utils = require('./testutils');
 
+// Start the rest server.
+// Currently the server can't be restarted for each test.
+var restServer = require('../server');
+
 describe('mongodb-rest', function () {
 
-    var restServer;
+    //todo: var restServer;
 
     beforeEach(function () {
 
-        // Load rest server.
-        restServer = require('../server');
+        // Open the rest server for each test.
+        //todo: restServer = require('../server');
     });
 
     afterEach(function () {
 
         // Close the rest server after each test.
-        restServer.server.close();
-        restServer = null;
+        //todo: restServer.server.close();
+        //todo: restServer = null;
     });
 
     it('should retreive empty array from empty db collection', function (done) {
@@ -46,4 +50,35 @@ describe('mongodb-rest', function () {
 
     });
 
+    it('can retreive array from db collection', function (done) {
+
+        var testData = [
+            {
+                item: 1,
+            },
+            {
+                item: 2,
+            },
+            {
+                item: 3,
+            },
+        ];
+
+        utils.loadFixture(testDbName, testCollectionName, testData, function () {
+
+            utils.requestJson(collectionUrl, function (err, result) {
+                if (err) {
+                    done(err);
+                    return;
+                }
+                
+                expect(result.length).toBe(3);
+                expect(result[0].item).toBe(1);
+                expect(result[1].item).toBe(2);
+                expect(result[2].item).toBe(3);
+                done();
+            });
+        });
+
+    });
 });
